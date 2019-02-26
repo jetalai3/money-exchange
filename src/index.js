864284693af122065fa668038601d746dd47beb4
jetalai3 <jetalai3@gmail.com>
2019-02-27 00:09:47 +0200

    Solve money exchange for 100%

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency>10000){
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }else if(currency<=0){
+        return {};
+    }
+    let h, q, d, n, p, rest;
+    h = Math.trunc(currency/50);
+    rest = currency - h*50;
+    q = Math.trunc(rest/25);
+    rest = rest - q*25;
+    d = Math.trunc(rest/10);
+    rest = rest - d*10;
+    n = Math.trunc(rest/5);
+    rest = rest - d*5;
+    p = currency - h*50 - q*25 - d*10 - n*5;
+    let result = {};
+    if(h>0){
+        result.H = h;
+    }
+    if(q>0){
+        result.Q = q;
+    }
+    if(d>0){
+        result.D = d;
+    }
+    if(n>0){
+        result.N = n;
+    }
+    if(p>0){
+        result.P = p;
+    }
+    return result;
 }
